5b63b1815298806de2c54ce594147df13ff6bb46
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2023-02-01 01:11:34 +0300

    1.1.8.4 update
    Small update

--- AminoXZ/__init__.py
+++ AminoXZ/__init__.py
@@ -17,7 +17,7 @@ __title__ = 'AminoXZ'
 __author__ = 'Xsarz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2022-2023 Xsarz'
-__version__ = '1.1.8.3'
+__version__ = '1.1.8'
 
 def init():
 	__newest__ = loads(get("https://pypi.org/pypi/aminoxz/json").text)["info"]["version"]
